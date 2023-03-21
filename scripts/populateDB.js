c362919c34a42eeec1447a515c17d5c28a7405a4
ben <phas0ruk@gmail.com>
2023-03-21 12:20:53 -0700

    adds running total for all stats

--- scripts/populateDB.js
+++ scripts/populateDB.js
@@ -1,18 +1,22 @@
 const { MongoClient } = require('mongodb');
 require("dotenv").config({ path: "../.env.local" });
 
+// const channels = [
+//     { channel: "Bankless", id: "UCAl9Ld79qaZxp9JzEOwd3aA" },
+//     { channel: "EllioTrades", id: "UCMtJYS0PrtiUwlk6zjGDEMA" },
+//     { channel: "BitBoy", id: "UCjemQfjaXAzA-95RKoy9n_g" },
+//     { channel: "CoinBureau", id: "UCqK_GSMbpiV8spgD3ZGloSw" },
+//     { channel: "AltcoinDaily", id: "UCbLhGKVY-bJPcawebgtNfbw" },
+//     { channel: "Lark Davis", id: "UCl2oCaw8hdR_kbqyqd2klIA" },
+//     { channel: "CTO Larsen", id: "UCFU-BE5HRJoudqIz1VDKlhQ" },
+//     { channel: "Ben Cowan", id: "UCRvqjQPSeaWn-uEx-w0XOIg" },
+//     { channel: "Altcoin Buzz", id: "UCGyqEtcGQQtXyUwvcy7Gmyg" },
+//     { channel: "JNRY Crypto", id: "UC188KLMYLLGqVJZdYq7mYFw" },
+//     { channel: "CryptosRUs", id: "UCI7M65p3A-D3P4v5qW8POxQ" },
+// ]
+
 const channels = [
-    { channel: "Bankless", id: "UCAl9Ld79qaZxp9JzEOwd3aA" },
-    { channel: "EllioTrades", id: "UCMtJYS0PrtiUwlk6zjGDEMA" },
-    { channel: "BitBoy", id: "UCjemQfjaXAzA-95RKoy9n_g" },
-    { channel: "CoinBureau", id: "UCqK_GSMbpiV8spgD3ZGloSw" },
-    { channel: "AltcoinDaily", id: "UCbLhGKVY-bJPcawebgtNfbw" },
-    { channel: "Lark Davis", id: "UCl2oCaw8hdR_kbqyqd2klIA" },
-    { channel: "CTO Larsen", id: "UCFU-BE5HRJoudqIz1VDKlhQ" },
-    { channel: "Ben Cowan", id: "UCRvqjQPSeaWn-uEx-w0XOIg" },
-    { channel: "Altcoin Buzz", id: "UCGyqEtcGQQtXyUwvcy7Gmyg" },
-    { channel: "JNRY Crypto", id: "UC188KLMYLLGqVJZdYq7mYFw" },
-    { channel: "CryptosRUs", id: "UCI7M65p3A-D3P4v5qW8POxQ" },
+  { channel: "Total", id: "na" },
 ]
 
 const connectionString = process.env.DB_STRING;

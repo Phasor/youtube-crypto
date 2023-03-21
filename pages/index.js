1b8f859896c4f0a114a0b9214dd4d5c1edbb5e96
ben <phas0ruk@gmail.com>
2023-03-21 14:54:30 -0700

    adds icon

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,7 @@ import Head from 'next/head'
 import Subscribers from '../components/Subscribers'
 import Views from '@/components/Views'
 import Videos from '@/components/Videos'
+import Image from 'next/image'
 
 export default function Home() {
 
@@ -14,13 +15,29 @@ export default function Home() {
       </Head>
       <div className='flex min-h-screen w-screen justify-center items-center overflow-x-hidden overflow-y-auto'>
         <div className='flex flex-col justify-center items-center'>
-          <h1 className=" text-4xl font-leckton font-semibold my-10 mx-auto">Crypto YouTube Barometer</h1>
+          <div className='flex space-x-3 justify-center items-center'>
+            <h1 className=" text-4xl font-leckton font-semibold my-10 mx-auto">Crypto YouTube Barometer</h1>
+            <Image
+              src='/meter.png'
+              alt='meter'
+              width={100}
+              height={100}
+              className="object-cover h-14 w-14 p-1"
+            />
+          </div>
 
-          <div className='grid grid-cols-1 md:grid-cols-2 max-w-6xl'>
+          <div className='grid grid-cols-1 md:grid-cols-2 max-w-6xl bg-gray-100 border rounded-lg shadow-lg mb-10'>
             {/* Text */}
-            <div className=' bg-gray-100'>
-              <h2 className='p-4 text-2xl font-leckton font-semibold mx-auto text-center'>What is This?</h2>
+            <div className=''>
+              <div>
+                <h2 className='p-4 text-2xl font-leckton font-semibold mx-auto text-center'>What is This?</h2>
+
+
+
+
+              </div>
               <p className='p-6 text-lg'>This is data dashboard looking at the total number of subscribers, views and videos put out by the biggest Crypto YouTube channels.</p>
+              <p className='px-6 text-lg'>The hope is that this will serve as a decent barometer for how hot the market is and therefore whether this is a good time to invest or not.</p>
             
               <h2 className='p-6 text-2xl font-leckton font-semibold mx-auto text-center'>What Crypto YT Channels are Included?</h2>
               <ul className='p-6 list-disc ml-10'>
@@ -41,13 +58,13 @@ export default function Home() {
               <p className='p-6 text-lg'>Once per day.</p>
 
               <h2 className='p-6 text-2xl font-leckton font-semibold mx-auto text-center'>Nice Site. Who's the Dev?</h2>
-              <p className='p-6 text-lg'>@Phas0r on Twitter.</p>
+              <a className='cursor-pointer' href="https://twitter.com/phas0r" target="_blank" rel="noopener noreferrer"><p className='p-6 text-lg underline text-blue-500'>@Phas0r</p></a>
 
             </div>
 
 
             {/* Graphs */}
-            <div className="flex flex-col items-center border-3 border-blue-500 w-full bg-gray-100">
+            <div className="flex flex-col items-center">
               <Subscribers/>
               <Views/>
               <Videos/>
